10e27a39ce7d2d7462d7509b483cb5906615805a
doridc06 <a_dorianaangelica.dacostamagello@iespablopicasso.es>
2023-03-31 09:58:26 +0200

    Agregando carpeta de Maven

---
+++ PruebaMaven/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Clase del main
+ */
+public class Main {
+    /**
+     * metodo del main
+     * @param args conjunto de argumentos
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
